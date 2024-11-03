26d536649865953d5e2c7ac070685b185a610d8e
Andriy Voloshyn bc2024-4 <ANDRII.VOLOSHYN@lnu.edu.ua>
2024-11-03 23:25:43 +0200

    added get method

--- main.js
+++ main.js
@@ -7,10 +7,35 @@ const superagent = require('superagent');
 program
 .requiredOption("-h, --host <host>, the server's adress")
 .requiredOption("-p, --port <port>, the server's port")
-.requiredOption("-c, --cache <cache>, the way to cashe files")
+.requiredOption("-c, --cache <cache>, the way to caсhe files")
 .parse(process.argv);
 
 const options = program.opts();
+
+async function requestListener(req, res) {
+    const urlParts = req.url.split('/');
+    const code = urlParts[1];
+    const filePath = `${cache}/${code}.jpg`;
+
+    switch (req.method) {
+        case 'GET':
+            try {                    
+                const image = await fs.readFile(filePath); // Спробуйте прочитати зображення з кешу
+                res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+                res.end(image); // Надіслати зображення, якщо знайдено
+            } catch (error) {
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.end('Image not found'); // Повернути повідомлення про те, що не знайдено
+            }
+            break;
+
+        
+
+
+
+
+
+
 
 const server = http.createServer(requestListener);
 
